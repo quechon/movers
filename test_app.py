bc68bbcd75ec1f8959040fcb6001725fa3a50d75
quechon <el.quechon@gmail.com>
2018-10-21 09:41:34 -0400

    Test app

--- test_app.py
+++ test_app.py
@@ -1,10 +1,11 @@
 import pytest
 from .app import app
+from .config import TestingConfig
 
 @pytest.fixture
 def client():
 
-    app.config['TESTING'] = True
+    app.config.from_object(TestingConfig)
 
     client = app.test_client()
 
@@ -16,3 +17,11 @@ def test_index(client):
     resp = client.get('/')
     assert resp.status_code == 200
     assert b'index' in resp.data
+    assert b'Jose Rodriguez' in resp.data
+
+def test_review_submit(client):
+
+    resp = client.post('api/review', json={'first_name': '', 'last_name': '',
+                                           'email': '', 'message': ''})
+
+    print(resp.get_json)

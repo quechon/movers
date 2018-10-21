eb639860676526de991dd580bfde707d5970733b
quechon <el.quechon@gmail.com>
2018-10-21 17:01:32 -0400

    Edit dummy reviews

--- main/view.py
+++ main/view.py
@@ -5,15 +5,14 @@ main = Blueprint('main', __name__)
 reviews = [
     [
     'Jose Rodriguez',
-    '3',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
     'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure' +
     'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' +
-    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'Sarasota, Fl'
     ],
     [
     'John Doe',
-    '5',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
     'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure' +
     'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' +
@@ -21,15 +20,17 @@ reviews = [
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
     'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure' +
     'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' +
-    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'Sarasota, Fl'
     ],
     [
     'Michael Robinson',
-    '4',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
     'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure' +
     'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' +
-    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'Sarasota, Fl'
+
     ]
 ]
 

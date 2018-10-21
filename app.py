7592aefc8e9acbe21175a7196e287163b8eba76e
quechon <el.quechon@gmail.com>
2018-10-21 09:36:24 -0400

    Added more extensions and switch to blueprints

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from .models import db
+from .form import csrf
+from .config import DevelopmentConfig
 
 # Initialize app
 app = Flask(__name__)
 
+app.config.from_object(DevelopmentConfig)
+
 # Initialize extentions
 db.init_app(app)
+csrf.init_app(app)
 
 
-# Include view 
-from movers import view
+# Include view
+from movers.main.view import main
+from movers.api.view import api
+
+app.register_blueprint(main)
+app.register_blueprint(api, url_prefix='/api')
